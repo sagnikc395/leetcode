e4c640b263881cb84468a182ddcef971d2ae131b
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-12-26 19:04:08 +0530

    src: added merge sort

---
+++ neetcode-all/array_hashing/mergeSortedArray.ts
@@ -0,0 +1,14 @@
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+     while (m > 0 && n > 0) {
+    if (nums1[m - 1] >= nums2[n - 1]) {
+      nums1[m + n - 1] = nums1[m - 1];
+      m -= 1;
+    } else {
+      nums1[m + n - 1] = nums2[n - 1];
+      n -= 1;
+    }
+  }
+  if (n > 0) {
+    nums1.splice(0, n, ...nums2.slice(0, n));
+  }
+};
