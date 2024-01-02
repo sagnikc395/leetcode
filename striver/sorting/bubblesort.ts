7b1ebe894265c9686a2310c644854429740a71ea
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-01-02 23:57:20 +0530

    src: added bubblesort

---
+++ striver/sorting/bubblesort.ts
@@ -0,0 +1,16 @@
+const bubbleSort = (arr: number[]) => {
+  let n = arr.length;
+  for (let i = n - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  console.log(arr);
+};
+
+bubbleSort([45,6,7,1,2]);
