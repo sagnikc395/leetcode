327a3b8244ede79ea9933603bf11f572cd53265e
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-01-09 20:33:21 +0530

    src: added a solution in cpp

--- daily-challenges/jan/jan8_rangeSumBST.ts
+++ daily-challenges/jan/jan8_rangeSumBST.ts
@@ -13,14 +13,19 @@
  */
 
 function rangeSumBST(root: TreeNode | null, low: number, high: number): number {
-    if(root===null){
-        return 0;
-    }
-    if(root.val > high){
-        return rangeSumBST(root.left,low,high);
-    }
-    if(root.val<low){
-        return rangeSumBST(root.right,low,high);
-    }
-    return root.val + rangeSumBST(root.left,low,high) + rangeSumBST(root.right,low,high);
-};
+  if (root === null) {
+    return 0;
+  }
+  if (root.val > high) {
+    return rangeSumBST(root.left, low, high);
+  }
+  if (root.val < low) {
+    return rangeSumBST(root.right, low, high);
+  }
+  return (
+    root.val +
+    rangeSumBST(root.left, low, high) +
+    rangeSumBST(root.right, low, high)
+  );
+}
+
