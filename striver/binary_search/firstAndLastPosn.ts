36cf3f7e780d479922d803adb26719a8cfe9270d
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-01-27 14:53:41 +0530

    src: added search in rotated sorted array

--- striver/binary_search/firstAndLastPosn.ts
+++ striver/binary_search/firstAndLastPosn.ts
@@ -36,3 +36,36 @@ function searchRange(nums: number[], target: number): number[] {
   }
   return [lb, upperBound(nums, target) - 1];
 }
+
+function firstAndLastBinarySearch(arr: number[], target: number): number[] {
+  let first = -1;
+  let low = 0;
+  let high = arr.length - 1;
+  let last = -1;
+  while (low <= high) {
+    let mid = Math.floor((first + last) / 2);
+    if (arr[mid] === target) {
+      first = mid;
+      high = mid - 1;
+    } else if (arr[mid] < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  low = 0;
+  high = arr.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((first + last) / 2);
+    if (arr[mid] === target) {
+      last = mid;
+      low = mid + 1;
+    } else if (arr[mid] < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return [first, last];
+}
