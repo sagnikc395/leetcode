56b61a27e80d15118c099c95381e1c1c6653dcbc
sagnik <sagnikchatterjee395@gmail.com>
2025-02-12 08:47:54 +0000

    src: solved p704

---
+++ src/striver/binary_search/p704.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/binary-search/description/
+
+class Solution:
+    def search(self, arr: List[int], target: int) -> int:
+        n = len(arr)
+        low = 0 
+        high = n - 1
+
+        while low <= high:
+            mid = (low+high)//2
+            if arr[mid] == target:
+                return mid 
+            elif arr[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return -1
+        
