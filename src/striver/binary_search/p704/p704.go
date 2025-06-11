70f365b35178378eeb7524a84dde41eeb84ea929
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-06-11 18:56:40 +0530

    added go solution

---
+++ src/striver/binary_search/p704/p704.go
@@ -0,0 +1,24 @@
+package main
+
+func search(nums []int, target int) int {
+	n := len(nums)
+	low := 0
+	high := n - 1
+
+	for low < high {
+		mid := (low + high) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+
+	return -1
+}
+
+func main() {
+
+}
