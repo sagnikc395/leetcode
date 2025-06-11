d106b851fcb7b62a3e12d3f7114aa4daeab4a490
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-06-11 19:01:30 +0530

    update

--- src/striver/binary_search/p704/p704.go
+++ src/striver/binary_search/p704/p704.go
@@ -1,22 +1,23 @@
 package main
 
-func search(nums []int, target int) int {
-	n := len(nums)
-	low := 0
-	high := n - 1
-
-	for low < high {
-		mid := (low + high) / 2
-		if nums[mid] == target {
-			return mid
-		} else if nums[mid] < target {
-			low = mid + 1
-		} else {
-			high = mid - 1
-		}
+func binarySearch(nums []int, low, high, target int) int {
+	if low > high {
+		return -1
 	}
 
-	return -1
+	mid := (low + high) / 2
+	if nums[mid] == target {
+		return mid
+	} else if target > nums[mid] {
+		return binarySearch(nums, mid+1, high, target)
+	} else {
+		return binarySearch(nums, low, mid-1, target)
+	}
+
+}
+
+func search(nums []int, target int) int {
+	return binarySearch(nums, 0, len(nums)-1, target)
 }
 
 func main() {
