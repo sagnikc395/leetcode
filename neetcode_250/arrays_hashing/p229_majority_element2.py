8f2b72834f9c7a02fe1aef7488af99a06a2b36e7
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2025-10-14 18:33:14 -0400

    feat: solved majority element2

---
+++ neetcode_250/arrays_hashing/p229_majority_element2.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        count = defaultdict(int)
+        for n in nums:
+            count[n] += 1
+            if len(count) < 3:
+                continue
+            
+            new_count = defaultdict(int)
+            for k, v in count.items():
+                if v > 1:
+                    new_count[k] = v - 1
+            count = new_count 
+        
+        res = []
+        for n in count:
+            if nums.count(n) > len(nums) // 3:
+                res.append(n)
+        return res
